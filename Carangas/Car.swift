3212be574635ed3d89c996f54752aa6ceda6f390
Vinicius Meira <viniciusmeirabr@hotmail.com>
2023-02-19 10:55:38 -0300

    Initial commit

---
+++ Carangas/Car.swift
@@ -0,0 +1,32 @@
+//
+//  Car.swift
+//  Carangas
+//
+//  Created by Vinicius Meira on 22/01/23.
+//  Copyright © 2023 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class Car: Codable {
+    var _id: String?
+    var brand: String = ""
+    var gasType: Int = 0
+    var name: String = ""
+    var price: Double = 0
+    
+    var gas: String {
+        switch gasType {
+            case 0:
+                return "Flex"
+            case 1:
+                return "Álcool"
+            default:
+                return "Gasolina"
+        }
+    }
+}
+
+struct Brand: Codable {
+    let nome: String
+}
---
+++ Carangas/REST.swift
@@ -0,0 +1,156 @@
+//
+//  Rest.swift
+//  Carangas
+//
+//  Created by Vinicius Meira on 22/01/23.
+//  Copyright © 2023 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
+enum RESTperation {
+    case save
+    case update
+    case delete
+}
+
+class REST {
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = false
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        config.timeoutIntervalForRequest = 30.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    }()
+    private static let session = URLSession(configuration: configuration)
+    
+    class func loadBrands(onComplete: @escaping ([Brand]?) -> Void) {
+        guard let url = URL(string: "https://parallelum.com.br/fipe/api/v1/carros/marcas") else {
+            onComplete(nil)
+            return
+        }
+        
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onComplete(nil)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else {
+                        onComplete(nil)
+                        return
+                    }
+                    do {
+                        let brands = try JSONDecoder().decode([Brand].self, from: data)
+                        onComplete(brands)
+                    } catch {
+                        onComplete(nil)
+                    }
+                } else {
+                    onComplete(nil)
+                }
+            } else {
+                onComplete(nil)
+            }
+        }
+        dataTask.resume()
+    }
+    
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                if response.statusCode == 200 {
+                    guard let data = data else {
+                        onError(.noData)
+                        return
+                    }
+                    do {
+                        let cars = try JSONDecoder().decode([Car].self, from: data)
+                        onComplete(cars)
+                    } catch {
+                        onError(.invalidJSON)
+                    }
+                } else {
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+            } else {
+                onError(.taskError(error: error!))
+            }
+        }
+        dataTask.resume()
+    }
+    
+    class func save(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .save, onComplete: onComplete)
+    }
+    
+    class func update(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .update, onComplete: onComplete)
+    }
+    
+    class func delete(car: Car, onComplete: @escaping (Bool) -> Void) {
+        applyOperation(car: car, operation: .delete, onComplete: onComplete)
+    }
+    
+    private class func applyOperation(car: Car, operation: RESTperation, onComplete: @escaping (Bool) -> Void) {
+        let urlString = basePath + "/" + (car._id ?? "")
+        
+        guard let url = URL(string: urlString) else {
+            onComplete(false)
+            return
+        }
+        var httpMethod: String
+        var request = URLRequest(url: url)
+        
+        switch operation {
+            case .save:
+                httpMethod = "POST"
+            case .update:
+                httpMethod = "PUT"
+            case .delete:
+                httpMethod = "DELETE"
+        }
+        
+        request.httpMethod = httpMethod
+        guard let json = try? JSONEncoder().encode(car) else {
+            onComplete(false)
+            return
+        }
+        request.httpBody = json
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if error == nil {
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200, let _ = data else {
+                    onComplete(false)
+                    return
+                }
+                onComplete(true)
+            } else {
+                onComplete(false)
+            }
+        }
+        dataTask.resume()
+    }
+}
